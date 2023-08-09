27fb63b9e60f7620ede168f93f650f314bb25da3
udit raj <uditraj3426@gmail.com>
2023-08-09 16:10:58 +0530

    - Socail Media Icon Button

---
+++ widgets/widget_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class WidgetColors {
+  // social media icon button colors
+  static const backgroundColor = Colors.white;
+  static final borderColor = Colors.grey.shade400;
+  static final assetInactiveColor = Colors.grey.shade400;
+}
